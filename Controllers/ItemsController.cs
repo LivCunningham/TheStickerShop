c931cef00659519b47a1df484a9028a831e4fe6b
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-23 12:52:02 -0400

    fixed database context issue

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using TheStickerShopApi.Models;
 using thestickershop;
-
+using TheStickerShopApi.Models;
 
 namespace TheStickerShopApi.Controllers
 
@@ -12,16 +11,14 @@ namespace TheStickerShopApi.Controllers
   [Route("api/[controller]")]
   public class ItemsController : ControllerBase
   {
-    private DatabaseContext db;
 
-    public ItemsController() => this.db = new DatabaseContext();
 
     [HttpPost]
     public ActionResult<StickerShopItems> Post([FromBody]StickerShopItems stickers)
     {
       //
       var db = new DatabaseContext();
-      db.StickerShopItems.Add(stickers);
+      db.StickerItems.Add(stickers);
       db.SaveChanges();
       return stickers;
 
